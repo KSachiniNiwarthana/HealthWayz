18bc1ee75bc26b11f45b0876430e7cfe6c5a0845
it21275074 <it21275074@my.sliit.lk>
2023-05-03 17:15:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/healthwayz/SupplierLogIn.kt
@@ -0,0 +1,11 @@
+package com.example.healthwayz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupplierLogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_supplier_log_in)
+    }
+}
